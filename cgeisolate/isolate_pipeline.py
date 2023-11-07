1b6ac1a7d9d555d776a77d8023a82a62d42e545a
MBHallgren <s153002@student.dtu.dk>
2023-11-07 11:57:08 +0100

    2

--- cgeisolate/isolate_pipeline.py
+++ cgeisolate/isolate_pipeline.py
@@ -6,9 +6,9 @@ from cgeisolate import kma
 
 def isolate_pipeline(args):
     # Check if kma is installed
-    #kma.KMARunner(args.input,
-    #          args.output + "/reference_mapping",
-    #          args.db_dir + '/bacteria_db',
-    #          "-ID 0 -nf -mem_mode -sasm -ef -1t1").run()
+    kma.KMARunner(args.input,
+              args.output + "/reference_mapping",
+              args.db_dir + '/bac_db',
+              "-ID 0 -nf -mem_mode -sasm -ef -1t1").run()
     print ('test')
     return 'isolate_pipeline'

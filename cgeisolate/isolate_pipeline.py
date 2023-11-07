e9b83b71bf1072a23648f49f45d4f9458d015606
MBHallgren <s153002@student.dtu.dk>
2023-11-07 11:58:54 +0100

    3

--- cgeisolate/isolate_pipeline.py
+++ cgeisolate/isolate_pipeline.py
@@ -5,10 +5,10 @@ import subprocess
 from cgeisolate import kma
 
 def isolate_pipeline(args):
+    os.system('mkdir ' + args.output)
     # Check if kma is installed
     kma.KMARunner(args.input,
               args.output + "/reference_mapping",
               args.db_dir + '/bac_db',
               "-ID 0 -nf -mem_mode -sasm -ef -1t1").run()
-    print ('test')
     return 'isolate_pipeline'

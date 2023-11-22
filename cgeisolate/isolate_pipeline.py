275c69e30d06eaa1f813b24f08192f6c19acf061
MBHallgren <s153002@student.dtu.dk>
2023-11-22 15:05:42 +0100

    meta26

--- cgeisolate/isolate_pipeline.py
+++ cgeisolate/isolate_pipeline.py
@@ -39,6 +39,9 @@ def isolate_pipeline(args):
         .format(args.input, args.output + "/mlst", args.db_dir)
     os.system(cmd)
 
+    report = create_report(args)
+    print (report)
+
     return 'isolate_pipeline'
 
 def get_highest_scoring_hit_template(file_path):
@@ -58,3 +61,50 @@ def get_highest_scoring_hit_template(file_path):
                 continue
 
     return highest_scoring_hit['#Template'] if highest_scoring_hit else None
+
+def read_tab_separated_file(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file, delimiter='\t')
+        return list(reader)
+
+def format_results_section(results, section_title):
+    report = f"{section_title}:\n"
+    report += "-" * 60 + "\n"
+    for result in results:
+        report += f"Template: {result['#Template']}\n"
+        report += f"Identity: {result['Template_Identity'].strip()}, Coverage: {result['Template_Coverage'].strip()}, Depth: {result['Depth'].strip()}\n\n"
+    return report
+
+def create_report(args):
+    # Load results
+    bacterial_results = read_tab_separated_file(args.output + "/bacteria_alignment.res")
+    amr_results = read_tab_separated_file(args.output + "/amr.res")
+    plasmid_results = read_tab_separated_file(args.output + "/plasmid.res")
+    highest_scoring_hit = get_highest_scoring_hit_template(args.output + "/bacteria_alignment.res")
+
+    report = "Pipeline Results Report\n"
+    report += "=" * 60 + "\n"
+
+    # Top Scoring Bacterial Alignment Hit
+    report += "Top Scoring Bacterial Alignment Hit:\n"
+    report += "-" * 60 + "\n"
+    report += f"Template: {highest_scoring_hit}\n\n"
+
+    # AMR Results Section
+    report += format_results_section(amr_results, "Antimicrobial Resistance (AMR) Findings")
+
+    # Plasmid Results Section
+    report += format_results_section(plasmid_results, "Plasmid Findings")
+
+    # Expected Phenotypes Based on AMR Genes Section (assuming this data is within amr_results)
+    report += "Expected Phenotypes Based on AMR Genes:\n"
+    report += "-" * 60 + "\n"
+    phenotypes = set()
+    for amr_result in amr_results:
+        phenotypes.update(amr_result['Phenotype'].split(','))  # Assuming 'Phenotype' is a field in amr_results
+    for phenotype in sorted(phenotypes):
+        report += f"• {phenotype.strip()}\n"
+    report += "\n"
+
+    return report
+
